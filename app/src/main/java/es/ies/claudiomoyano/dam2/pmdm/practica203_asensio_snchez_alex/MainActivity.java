a2e9e2443899bed152ab013ae1c4e168646b63fa
Alex <alexasensio04clase@gmail.com>
2025-10-01 11:08:06 +0200

    Avances

--- app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practica203_asensio_snchez_alex/MainActivity.java
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practica203_asensio_snchez_alex/MainActivity.java
@@ -1,6 +1,10 @@
 package es.ies.claudiomoyano.dam2.pmdm.practica203_asensio_snchez_alex;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        CheckBox activadorBoton = findViewById(R.id.checkBox);
+        Button boton = findViewById(R.id.button);
+        TextView texto = findViewById(R.id.textView);
+
+        boton.setEnabled(false);
+
+        activadorBoton.setOnCheckedChangeListener(new View.onCheckedChangeListener(){
+            @Override
+            public void onCheckedChange(View v){
+                if(boton.isEnabled()){
+                    boton.setEnabled(false);
+                }
+                else{
+                    boton.setEnabled(true);
+                }
+            }
         });
     }
 }
